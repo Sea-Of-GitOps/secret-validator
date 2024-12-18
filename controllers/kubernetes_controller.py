2284904043541d52ff3f978c5e7c790bbe29f8f3
Luca Maggio <lucamaggio1992@gmail.com>
2024-12-18 01:31:54 +0100

    feat(pod_service): delete useless service

--- controllers/kubernetes_controller.py
+++ controllers/kubernetes_controller.py
@@ -1,5 +1,4 @@
 from services.secret_service import SecretService
-from services.pod_service import PodService
 from services.kubernetes_service import KubernetesService
 
 
@@ -8,15 +7,6 @@ class KubernetesController:
         self._logger = logger
         self._k8s_service = KubernetesService(logger)
         self._secret_service = SecretService(self._k8s_service, logger)
-        self._pod_service = PodService(self._k8s_service, logger)
-
-    def get_pods(self, namespace: str):
-        self._logger.info(
-            "Request received to get pods "
-            f"existence with namespace: {namespace}"
-        )
-
-        self._pod_service.get_pods(namespace)
 
     def check_secret_existence(self, namespace: str, secret_name: str):
         self._logger.info(
